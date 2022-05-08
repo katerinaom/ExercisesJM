c27b7ff2b85b15dd4ebd9638bf4b906fde0922ec
JekaterinaM <katerinaom@inbox.lv>
2022-05-08 20:09:29 +0300

    Exersies 1

--- Exercises/Level1/Exercises.Level1/Logic1.cs
+++ Exercises/Level1/Exercises.Level1/Logic1.cs
@@ -19,7 +19,16 @@ public class Logic1
     /// </summary>
     public bool CigarParty(int cigars, bool isWeekend)
     {
-        throw new NotImplementedException();
+        // 1. Ja cigāru skaits ir zem 40 tad atgrižam false
+        // 2. Ja ir brīvdiena tad atgriežam vērtību true
+        // 3. Ja cigāru skaits ir zem 60 tad atgriežam vērtibu true (zinām jau, ka nav brivdiena)
+        if (cigars >= 40 && (isWeekend || cigars <=60)) 
+        {
+            return true;
+        }
+
+        // 4. Visos citos gadijumis atgriežam false
+        return false;
     }
 
     /// <summary>
@@ -36,7 +45,23 @@ public class Logic1
     /// </summary>
     public int DateFashion(int you, int date)
     {
-        throw new NotImplementedException();
+        // 1. Ja kāds ir 2 vai mazak, tad atgriežam vērtību 0
+        if (you <= 2 || date <= 2)
+        
+        {
+            return 0;
+        }
+
+        // 2. Ja kāds ir ar 8 vai vairāk, tad atgriežam vērtību 2
+        if (you >= 8 || date >= 8) 
+        
+        {
+            return 2;
+        }
+
+        //3. Citādi atgriežam vērtību 1
+        return 1;
+
     }
 
     /// <summary>
@@ -51,7 +76,13 @@ public class Logic1
     /// </summary>
     public bool SquirrelPlay(int temp, bool isSummer)
     {
-        throw new NotImplementedException();
+        // 1. Ja temperatura ir virs 60 un zem 90 (nav vasara) tad atgriežam vērtību true
+
+        // 2. Ja temperatūra ir virs 60 un zem 100 un ir vasara tad atgriežam vērtibu true
+
+        // 3.  Citadi false
+       
+        return false;
     }
 
     /// <summary>
